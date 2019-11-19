2bbc7b26bd208ec3006019d179629931a62706aa
kishore-lendingwise <kishore@lendingwise.com>
2019-11-19 18:08:51 +0530

    Added 100 year calculation and check running pid, ppid, Owner and time started

---
+++ 100_years.py
@@ -0,0 +1,4 @@
+name = raw_input ("What is your name?")
+age = raw_input ("How old are you?")
+year = str((2019 - %r)+100) %age
+print "So you're %r, %r years old, will be 100 at %r" %(name, age, year)
